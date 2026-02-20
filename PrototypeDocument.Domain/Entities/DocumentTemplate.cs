adf2823cf79fcd3bb5d33aac4682ae47c07385cc
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-19 23:56:22 -0300

    add class

---
+++ PrototypeDocument.Domain/Entities/DocumentTemplate.cs
@@ -0,0 +1,21 @@
+﻿namespace PrototypeDocument.Domain.Entities;
+
+public class DocumentTemplate
+{
+    public string Title { get; set; }
+    public string Category { get; set; }
+    public List<Section> Sections { get; set; }
+    public DocumentStyle Style { get; set; }
+    public List<string> RequiredFields { get; set; }
+    public Dictionary<string, string> Metadata { get; set; }
+    public ApprovalWorkflow Workflow { get; set; }
+    public List<string> Tags { get; set; }
+
+    public DocumentTemplate()
+    {
+        Sections = new List<Section>();
+        RequiredFields = new List<string>();
+        Metadata = new Dictionary<string, string>();
+        Tags = new List<string>();
+    }
+}
