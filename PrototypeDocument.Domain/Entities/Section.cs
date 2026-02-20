58be90fa8739e8eb033ab772fcc1c64e2ed5ebe7
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-19 23:56:44 -0300

    add class

---
+++ PrototypeDocument.Domain/Entities/Section.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Principal;
+
+namespace PrototypeDocument.Domain.Entities;
+
+public class Section
+{
+    public string Name { get; set; }
+    public string Content { get; set; }
+    public bool IsEditable { get; set; }
+    public List<string> Placeholders { get; set; }
+
+    public Section()
+    {
+        Placeholders = new List<string>();
+    }
+}
