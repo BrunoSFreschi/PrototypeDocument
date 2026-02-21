6f383256de31ee249af4c2e0f7ea53075aa7cb7f
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-21 10:29:35 -0300

    removed

--- PrototypeDocument.Domain/Entities/Margins.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrototypeDocument.Domain.Entities;
-
-public class Margins
-{
-    public int Top { get; set; }
-    public int Bottom { get; set; }
-    public int Left { get; set; }
-    public int Right { get; set; }
-}
