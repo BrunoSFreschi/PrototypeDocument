88d27da07db76356f8c46070c2cefb6e726cec3d
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-21 10:30:03 -0300

    add interface

---
+++ PrototypeDocument.Domain/Abstractions/INotificationChannel.cs
@@ -0,0 +1,11 @@
+﻿using PrototypeDocument.Domain.Models;
+
+namespace PrototypeDocument.Domain.Abstractions;
+
+public interface INotificationChannel
+{
+    string ChannelType { get; }
+
+
+    void Send(NotificationMessage message);
+}
