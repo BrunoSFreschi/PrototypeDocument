568b2fb502d6742008f3b0ff47aa4c683dee197d
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-21 10:29:55 -0300

    create models

---
+++ PrototypeDocument.Domain/Models/NotificationMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace PrototypeDocument.Domain.Models;
+
+public class NotificationMessage
+{
+    public string Recipient { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
