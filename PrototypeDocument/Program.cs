73e49e28c97b22321f723a6ebb66f088cb393797
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-23 23:23:04 -0300

    update

--- PrototypeDocument/Program.cs
+++ PrototypeDocument/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-z
+﻿using Microsoft.Extensions.DependencyInjection;
+using PrototypeDocument.Domain.Abstractions;
+using PrototypeDocument.Domain.Enums;
+using PrototypeDocument.Infrastructure.Notifications;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<INotificationChannel, EmailChannel>();
+services.AddSingleton<INotificationChannel, SmsChannel>();
+services.AddSingleton<INotificationChannel, PushChannel>();
+services.AddSingleton<INotificationChannel, WhatsAppChannel>();
+
+services.AddSingleton<NotificationManager>();
+
+var provider = services.BuildServiceProvider();
+var manager = provider.GetRequiredService<NotificationManager>();
+
+manager.SendOrderConfirmation("cliente@email.com", "12345", NotificationType.Email);
+Console.WriteLine();
+manager.SendOrderConfirmation("+5511999999999", "12346", NotificationType.Sms);
+Console.WriteLine();
+manager.SendShippingUpdate("device-token-abc", "BR123456789", NotificationType.Push);
